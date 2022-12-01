04543d40ee3abc346744684470486d4be1012cc4
Husam Hilal <husamhilal@hotmail.com>
2022-11-30 21:23:58 -0500

    updating labdeploy parameters section

--- labdeploy.ps1
+++ labdeploy.ps1
@@ -10,14 +10,14 @@
 
 param (
     [Parameter(Mandatory)]
-    [ValidateSet("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30",ErrorMessage="Value '{0}' is invalid. The allowed ranges is between 1 to 30. Try one of: '{1}'")]
+    [ValidateRange(1,50)]
     [Alias("GroupNumber")] 
-    [string] $group,
+    [Int] $group,
     
     [Parameter(Mandatory)]
-    [ValidateSet("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30",ErrorMessage="Value '{0}' is invalid. The allowed ranges is between 1 to 30. Try one of: '{1}'")]
+    [ValidateRange(1,50)]
     [Alias("LabNumber")] 
-    [string] $lab,
+    [Int] $lab,
 
     [Parameter()]
     [Alias("IsAutomated")] 
